bb31b70d57fc9f44fe82fe336c3eece2b935836d
DAMunene <muriukimoff6@gmail.com>
2024-09-02 06:07:44 +0300

    - GET, POST, PUT and DELETE using Class based functions views

--- ToDo/kaizen/urls.py
+++ ToDo/kaizen/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path 
-from .views import snippet_list, snippet_detail
+from .views import  SnippetList, SnippetDetail
 from rest_framework.urlpatterns import format_suffix_patterns
+from . import views
 
+# urlpatterns = [
+#     path('snippets/', snippet_list, name='snippet_list'),
+#     path('snippets/<int:pk>/', snippet_detail, name='snippet_detail'),
 
+# ]
 urlpatterns = [
-    path('snippets/', snippet_list, name='snippet_list'),
-    path('snippets/<int:pk>/', snippet_detail, name='snippet_detail'),
+    path('snippets/', SnippetList.as_view(), name='snippet_list'),
+    path('snippets/<int:pk>/', SnippetDetail.as_view(), name='snippet_detail'),
 
 ]
 urlpatterns = format_suffix_patterns(urlpatterns)
